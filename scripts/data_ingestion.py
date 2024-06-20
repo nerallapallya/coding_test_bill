3fb992c336979c0450fdc126bc84550df06684e6
nerallapallya <146959916+nerallapallya@users.noreply.github.com>
2024-06-20 14:55:11 -0400

    Update data_ingestion.py

--- scripts/data_ingestion.py
+++ scripts/data_ingestion.py
@@ -1,60 +1,51 @@
 import os
-from datetime import datetime
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import func
+import glob
+import datetime
 import logging
-from api.weather_data_models import weather_data_model
+from api.weather_data_models.weather_data_model import weather_data_input, db
+from api.api_setup import app
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Connect to the database
-engine = create_engine('sqlite:///weather_inputs.db')
-Session = sessionmaker(bind=engine)
-session = Session()
+def parse_line(line):
+    parts = line.strip().split("\t")
+    date = datetime.datetime.strptime(parts[0], '%Y%m%d').date()
+    maximum_temp = int(parts[1]) / 10.0 if int(parts[1]) != -9999 else None
+    minimum_temp = int(parts[2]) / 10.0 if int(parts[2]) != -9999 else None
+    precipitation = int(parts[3]) / 10.0 if int(parts[3]) != -9999 else None
+    return date, maximum_temp, minimum_temp, precipitation
 
 
-def ingest_weather_inputs(file_path, station_id):
-    with open(file_path, 'r') as file:
-        for line in file:
-            parts = line.strip().split('\t')
-            date_input, maximum_temp, minimum_temp, precip = parts
-            date = datetime.strptime(date_input, '%Y%m%d').date()
-            maximum_temp = None if int(maximum_temp) == -9999 else int(maximum_temp) / 10.0
-            minimum_temp = None if int(minimum_temp) == -9999 else int(minimum_temp) / 10.0
-            precipitation = None if int(precip) == -9999 else int(precip) / 10.0
+def ingest_weather_inputs(dir):
+    with app.app_context():
+        files = glob.glob(os.path.join(dir, '*.txt'))
+        total_records = 0
+        start_time = datetime.datetime.now()
 
-            existing_record = session.query(weather_data_model.weather_data_input).filter_by(station_id=station_id,
-                                                                                             date=date).first()
+        for file in files:
+            station_id = os.path.basename(file).split('.')[0]
+            with open(file, 'r') as f:
+                for line in f:
+                    date, maximum_temp, minimum_temp, precipitation = parse_line(line)
+                    if not db.session.query(weather_data_input).filter_by(station_id=station_id, date=date).first():
+                        weather_record = weather_data_input(
+                            station_id=station_id,
+                            date=date,
+                            maximum_temp=maximum_temp,
+                            minimum_temp=minimum_temp,
+                            precipitation=precipitation
+                        )
+                        db.session.add(weather_record)
+                        total_records += 1
+            db.session.commit()
 
-            if existing_record:
-                # Update existing record
-                existing_record.maximum_temp = maximum_temp
-                existing_record.minimum_temp = minimum_temp
-                existing_record.precipitation = precipitation
-            else:
-                # Insert new record
-                new_record = weather_data_model.weather_data_input(
-                    station_id=station_id, date=date, maximum_temp=maximum_temp, minimum_temp=minimum_temp, precipitation=precipitation)
-                session.add(new_record)
+        end_time = datetime.datetime.now()
+        logger.info(f"Ingestion started at {start_time} and ended at {end_time}")
+        logger.info(f"Total records ingested: {total_records}")
 
-    session.commit()
-    session.close()
 
-if __name__ == '__main__':
-    logger.info("Begin data ingestion")
-    start_time = datetime.now()
-
-    wx_data_dir = '../code-challenge-template/wx_data'
-    for file_name in os.listdir(wx_data_dir):
-        station_id = file_name.split('.')[0]
-        file_path = os.path.join(wx_data_dir, file_name)
-        ingest_weather_inputs(file_path, station_id)
-
-    end_time = datetime.now()
-    logger.info(f"Data ingestion finished")
-    logger.info(f"start time: {start_time}")
-    logger.info(f"end time: {end_time}")
+if __name__ == "__main__":
+    ingest_weather_inputs('../input_data/wx_data')
 
