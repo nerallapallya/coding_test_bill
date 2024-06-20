9948a104c002d64fb1efa6e41670fef4aceddedf
nerallapallya <146959916+nerallapallya@users.noreply.github.com>
2024-06-20 14:13:27 -0400

    Update weather_data_model.py
    updated the models script to use SQLAlchemy

--- api/weather_data_models/weather_data_model.py
+++ api/weather_data_models/weather_data_model.py
@@ -1,32 +1,29 @@
-from sqlalchemy import Column, Integer, String, Float, Date, create_engine, UniqueConstraint
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from flask_sqlalchemy import SQLAlchemy
 
-Base = declarative_base()
+db = SQLAlchemy()
 
-class weather_data_input(Base):
+
+class weather_data_input(db.Model):
     __tablename__ = 'weather_data'
 
-    id = Column(Integer, primary_key=True)
-    station_id = Column(String, nullable=False)
-    date = Column(Date, nullable=False)
-    maximum_temp = Column(Float, nullable=True)
-    minimum_temp = Column(Float, nullable=True)
-    precipitation = Column(Float, nullable=True)
+    id = db.Column(db.Integer, primary_key=True)
+    station_id = db.Column(db.String, nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    maximum_temp = db.Column(db.Float)
+    minimum_temp = db.Column(db.Float)
+    precipitation = db.Column(db.Float)
 
-    __table_args__ = (UniqueConstraint('station_id', 'date', name='_station_date_uc'),)
+    __table_args__ = (db.UniqueConstraint('station_id', 'date', name='_station_date_uc'),)
 
 
-class weather_stats_output(Base):
+class weather_data_stats(db.Model):
     __tablename__ = 'weather_stats'
-    id = Column(Integer, primary_key=True)
-    station_id = Column(String, nullable=False)
-    year = Column(Integer, nullable=False)
-    avg_maximum_temp = Column(Float, nullable=True)
-    avg_minimum_temp = Column(Float, nullable=True)
-    total_precipitation = Column(Float, nullable=True)
 
-# Create SQLite database
-engine = create_engine('sqlite:///weather_inputs.db')
-Base.metadata.drop_all(engine)
-Base.metadata.create_all(engine)
+    id = db.Column(db.Integer, primary_key=True)
+    station_id = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    avg_maximum_temp = db.Column(db.Float)
+    avg_minimum_temp = db.Column(db.Float)
+    total_precipitation = db.Column(db.Float)
+
+    __table_args__ = (db.UniqueConstraint('station_id', 'year', name='_station_year_uc'),)
