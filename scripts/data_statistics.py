02469090f81ac3f5def160a755ccf26ba2dc2536
nerallapallya <146959916+nerallapallya@users.noreply.github.com>
2024-06-20 15:02:10 -0400

    Update data_statistics.py

--- scripts/data_statistics.py
+++ scripts/data_statistics.py
@@ -1,50 +1,52 @@
-import os
-from datetime import datetime
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import func
+import datetime
+from sqlalchemy.sql import func
 import logging
-from api.weather_data_models import weather_data_model
+from api.weather_data_models.weather_data_model import db, weather_data_input, weather_data_stats
+from api.api_setup import app
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Connect to the database
-engine = create_engine('sqlite:///weather_inputs.db')
-Session = sessionmaker(bind=engine)
-session = Session()
-
 
 def calculate_aggregate_stats():
-    weather_data = session.query(
-        weather_data_model.weather_data_input.unique_id,
-        func.strftime('%Y', weather_data_model.weather_data_input.date).label('year'),
-        func.avg(weather_data_model.weather_data_input.maximum_temp).label('avg_maximum_temp'),
-        func.avg(weather_data_model.weather_data_input.minimum_temp).label('avg_minimum_temp'),
-        func.sum(weather_data_model.weather_data_input.precipitation).label('total_precipitation')).group_by(
-        func.strftime('%Y', weather_data_model.weather_data_input.date)).all()
+    with app.app_context():
+        stats_data = db.session.query(
+            weather_data_input.station_id,
+            func.strftime('%Y', weather_data_input.date).label('year'),
+            func.avg(weather_data_input.maximum_temp).label('avg_maximum_temp'),
+            func.avg(weather_data_input.minimum_temp).label('avg_minimum_temp'),
+            func.sum(weather_data_input.precipitation).label('total_precipitation')
+        ).group_by(
+            weather_data_input.station_id,
+            func.strftime('%Y', weather_data_input.date)
+        ).all()
 
-    for data in weather_data:
-        weather_stat = weather_data_model.weather_stats_output(
-            station_id=data.station_id,
-            year=int(data.year),
-            avg_maximum_temp=data.avg_maximum_temp,
-            avg_minimum_temp=data.avg_minimum_temp,
-            total_precipitation=data.total_precipitation
-        )
-        session.add(weather_stat)
+    for data in stats_data:
+        year = int(data.year)
+        avg_maximum_temp = round(data.avg_maximum_temp, 2) if data.avg_maximum_temp is not None else None
+        avg_minimum_temp = round(data.avg_minimum_temp, 2) if data.avg_minimum_temp is not None else None
+        total_precipitation = round(data.total_precipitation / 10, 2) if data.total_precipitation is not None else None
 
-    session.commit()
-    session.close()
+        existing_stat = weather_data_stats.query.filter_by(station_id=data.station_id, year=year).first()
+
+        if not existing_stat:
+            weather_stat = weather_data_stats(
+                station_id=data.station_id,
+                year=year,
+                avg_max_temp=avg_maximum_temp,
+                avg_min_temp=avg_minimum_temp,
+                total_precipitation=total_precipitation
+            )
+            db.session.add(weather_stat)
+        else:
+            existing_stat.avg_max_temp = avg_maximum_temp
+            existing_stat.avg_min_temp = avg_minimum_temp
+            existing_stat.total_precipitation = total_precipitation
+
+    db.session.commit()
+    print("Weather statistics calculated and stored.")
 
 
-if __name__ == '__main__':
-    logger.info("Begin data ingestion")
-    start_time = datetime.now()
-
+if __name__ == "__main__":
     calculate_aggregate_stats()
-    logger.info(f"aggregation finished")
-    logger.info(f"start time: {start_time}")
-    end_time = datetime.now()
-    logger.info(f"end time: {end_time}")
